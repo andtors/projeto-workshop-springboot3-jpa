8b38537ce10600d401d51e166b2d970c1e2ef2a9
andtors <andre.tors448@gmail.com>
2023-12-26 11:53:27 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resource/UserResource.java
+++ src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resource;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 /*Para cada classe que formos criar que contenha objetos, vamos criar uma subclasse para que possamos fazer a conexão entre o banco de dados e o programa utilizando as
  * sintaxes do SpringBoot*/
@@ -16,10 +20,19 @@ import com.educandoweb.course.entities.User;
 @RequestMapping(value = "/user")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
 
