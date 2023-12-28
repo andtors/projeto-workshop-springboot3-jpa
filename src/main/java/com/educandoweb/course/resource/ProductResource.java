062bcc09e4e84c3144a962c590e554f01ab04a31
andtors <andre.tors448@gmail.com>
2023-12-28 10:37:42 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/resource/ProductResource.java
@@ -0,0 +1,38 @@
+package com.educandoweb.course.resource;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.services.ProductService;
+
+/*Para cada classe que formos criar que contenha objetos, vamos criar uma subclasse para que possamos fazer a conexão entre o banco de dados e o programa utilizando as
+ * sintaxes do SpringBoot*/
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
+
